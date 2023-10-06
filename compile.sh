04e8b2d2cbc3382c282c334dd8c1034e0a7718f5
Franco Morroni <fmorroni99@gmail.com>
2023-10-06 16:27:50 -0300

    feat: Change ownership of qemu image to current user after compilation

--- compile.sh
+++ compile.sh
@@ -3,9 +3,10 @@
 container='arqui_builder'
 docker='sudo docker'
 
-$docker start $container;
-$docker exec -it $container make clean -C/root/Toolchain/;
-$docker exec -it $container make clean -C/root/;
-$docker exec -it $container make -C/root/Toolchain/;
-$docker exec -it $container  make -C/root/;
-$docker stop $container;
+$docker start $container
+$docker exec -it $container make clean -C/root/Toolchain/
+$docker exec -it $container make clean -C/root/
+$docker exec -it $container make -C/root/Toolchain/
+$docker exec -it $container  make -C/root/
+$docker stop $container
+sudo chown $USER:$USER ./Image/x64BareBonesImage.qcow2
