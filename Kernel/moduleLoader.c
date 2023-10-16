58fc8a0b4d3e6c242fe2976acd80d3cd767fc8dd
Franco Morroni <fmorroni99@gmail.com>
2023-10-16 20:09:17 -0300

    chore: Comments trying to understand the linker script and moduleLoader

--- Kernel/moduleLoader.c
+++ Kernel/moduleLoader.c
@@ -6,10 +6,13 @@
 static void loadModule(uint8_t ** module, void * targetModuleAddress);
 static uint32_t readUint32(uint8_t ** address);
 
+// 	 loadModules(&endOfKernelBinary, moduleAddresses);
 void loadModules(void * payloadStart, void ** targetModuleAddress)
 {
 	int i;
+  // currentModule -> endOfKernelBinary
 	uint8_t * currentModule = (uint8_t*)payloadStart;
+  // moduleCount = endOfKernelBinary
 	uint32_t moduleCount = readUint32(&currentModule);
 
 	for (i = 0; i < moduleCount; i++)
@@ -37,7 +40,12 @@ static void loadModule(uint8_t ** module, void * targetModuleAddress)
 
 static uint32_t readUint32(uint8_t ** address)
 {
+  // address -> currentModule -> endOfKernelBinary
+  // *address = currentModule -> endOfKernelBinary
+  // result = **address = endOfKernelBinary = 2???
 	uint32_t result = *(uint32_t*)(*address);
+  // *address = currentModule -> endOfKernelBinary
 	*address += sizeof(uint32_t);
+  // *address = currentModule + 32 bits = &endOfKernelBinary + 32 bits
 	return result;
 }
