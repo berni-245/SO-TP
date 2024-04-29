96e08ec90ed2f99758220eb4ef380e296a9d9c20
Franco Morroni <fmorroni99@gmail.com>
2024-04-29 20:52:34 -0300

    chore: better shell scripting

--- compile.sh
+++ compile.sh
@@ -3,14 +3,13 @@
 container='arqui_builder'
 docker='sudo docker'
 
-$docker start $container
-if [[ $? -ne 0 ]]; then
-  exit $?
+if ! $docker start $container; then
+	exit $?
 fi
 $docker exec -it $container make -C /root/ clean
 $docker exec -it $container make -C /root/ all
 $docker stop $container
-sudo chown $USER:$USER ./Image/x64BareBonesImage.qcow2
+sudo chown "$USER:$USER" ./Image/x64BareBonesImage.qcow2
 
 cat <<EOF > Kernel/.clangd
 CompileFlags:
