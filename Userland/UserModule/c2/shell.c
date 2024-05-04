149a956037bec8c5c038c55aa9c599b1d86388e3
Franco Morroni <fmorroni99@gmail.com>
2024-05-04 00:07:23 -0300

    fix: include shellUtils directly in shell.c
    It made no sense to have it in shell.h, the whole reason to put utils in a separate header
    file was so that including shell.h only includes the shell() function.

--- Userland/UserModule/c2/shell.c
+++ Userland/UserModule/c2/shell.c
@@ -1,4 +1,4 @@
-#include <shell.h>
+#include <shellUtils.h>
 
 extern uint8_t bss;
 
--- Userland/UserModule/include/shell.h
+++ Userland/UserModule/include/shell.h
@@ -1,10 +1,6 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <shellUtils.h>
-
-extern int currentCommandIdx;
-
 int shell();
 
 #endif
