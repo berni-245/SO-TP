21969ea611c73bb0b72c8d4e37298d7cd5cc7e44
Berni245 <bernizapico@gmail.com>
2024-06-06 20:43:45 -0300

    Fix: changed logic of buddy system implementation so it actually merges recursively

--- Kernel/c/buddySystem.c
+++ Kernel/c/buddySystem.c
@@ -10,8 +10,10 @@ typedef enum { false = 0, true = 1 } boolean;
 static const uint64_t addressByteSize = sizeof(void*);
 
 typedef struct Block {
+    struct Block* currentBuddy;
+    struct Block* nextBuddy;
     struct Block* next;
-    unsigned int size;
+    uint32_t size;
     boolean isFree;
 } Block;
 
@@ -25,15 +27,17 @@ void initBuddySystem(void* endOfModules) {
     Block* initialBlock = (Block*) (((uint64_t)endOfModules + addressByteSize - 1) & ~(addressByteSize - 1));
     initialBlock->size = MAX_MEMORY_AVAILABLE;
     initialBlock->isFree = true;
+    initialBlock->currentBuddy = NULL;
+    initialBlock->nextBuddy = NULL;
     initialBlock->next = NULL;
     freeList[ORDER_COUNT - 1] = initialBlock;
 }
 
-static int getOrder(unsigned int size) {
+static int getOrder(uint32_t size) {
     // Part of the memory of the block will contain the structure
     size += sizeof(Block); 
-    int order = 0;
-    uint64_t iniBlockSize = 1;
+    uint32_t order = 0;
+    uint32_t iniBlockSize = 1;
     while (iniBlockSize < size) {
         iniBlockSize <<= 1;
         order++;
@@ -42,27 +46,31 @@ static int getOrder(unsigned int size) {
 }
 
 static Block* splitBlock(Block* block) {
-    block->size /= 2;
+    uint32_t newBlockSize = (block->size)/2;
+    block->size = newBlockSize;
 
     // Get the direction of the buddy
-    Block* buddy = (Block*)((char*)block + block->size);
-    buddy->size = block->size;
+    Block* buddy = (Block*)((char*)block + newBlockSize);
+    buddy->size = newBlockSize;
     buddy->isFree = true;
-    buddy->next = NULL;
 
-    block->next = buddy;
+    buddy->nextBuddy = NULL;
+    buddy->currentBuddy = block;
+
+    block->nextBuddy = block->currentBuddy;
+    block->currentBuddy = buddy;
     return buddy;
 }
 
-void* buddyAlloc(unsigned int size) {
+void* buddyAlloc(uint32_t size) {
     int order = getOrder(size);
 
     for (int currentOrder = order; currentOrder < ORDER_COUNT; currentOrder++) {
-        if (freeList[currentOrder] != NULL) {
+        if (freeList[currentOrder] != NULL && freeList[currentOrder]->isFree) { // TODO: maybe remove isFree
             Block* block = freeList[currentOrder];
             freeList[currentOrder] = block->next;
 
-            while (currentOrder > order) {
+            while (order < currentOrder) {
                 currentOrder--;
                 Block* buddy = splitBlock(block);
                 buddy->next = freeList[currentOrder];
@@ -77,24 +85,38 @@ void* buddyAlloc(unsigned int size) {
     return NULL;
 }
 
-static void mergeBlock(Block* block, int order) {
-    unsigned int blockSize = block->size;
-    uintptr_t buddyAddress = (uintptr_t)block ^ blockSize;
-    Block* buddy = (Block*)buddyAddress;
-
-    if ((char*)buddy < (char*)freeList[order]) return;
-    if (!buddy->isFree) return;
-    if (buddy->size != blockSize) return;
-
-    if (buddy < block) {
-        Block* temp = block;
-        block = buddy;
-        buddy = temp;
+static void removeFromFreeList(Block* toRemove, uint32_t order) {
+    Block* previousBlock = freeList[order];
+    if(previousBlock == toRemove) {
+        freeList[order] = previousBlock->next;
+        return;
     }
 
-    block->size *= 2;
-    block->next = buddy->next;
+    Block* currentBlock = previousBlock->next;
+    while(currentBlock != toRemove) { // if we ever get a NULL, then there was a programming error somewhere
+        previousBlock = currentBlock;
+        currentBlock = currentBlock->next;        
+    }
+    previousBlock->next = currentBlock->next;
+}
+
+static void mergeBlock(Block* block, uint32_t order) {
+    block->next = freeList[order];
     freeList[order] = block;
+
+    if(block->size == MAX_MEMORY_AVAILABLE) return;
+
+    Block* buddy = block->currentBuddy;
+
+    if (!buddy->isFree) return;
+    if (buddy->size != block->size) return;
+
+    freeList[order] = block->next; // remove the block from the free list
+    removeFromFreeList(buddy, order); // remove the buddy from the free list
+
+    block->currentBuddy = block->nextBuddy;
+    block->nextBuddy = block->currentBuddy->next;
+    block->size *= 2;
 
     mergeBlock(block, order + 1);
 }
@@ -105,9 +127,6 @@ void buddyFree(void* ptr) {
     Block* block = (Block*)ptr - 1;  // The block structure will be before the address returned to the user
     block->isFree = true;
     int order = getOrder(block->size - sizeof(Block)); // TODO: use a variable in block for requested size
-
-    block->next = freeList[order];
-    freeList[order] = block;
 
     mergeBlock(block, order);
 }
