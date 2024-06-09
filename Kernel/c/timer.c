9c627104a81149956569a8484f3457b4fdf80594
Franco Morroni <fmorroni99@gmail.com>
2024-06-09 19:59:28 -0300

    chore: formatting

--- Kernel/c/timer.c
+++ Kernel/c/timer.c
@@ -1,46 +1,45 @@
 #include <interruptions.h>
-#include <timer.h>
-#include <scheduler.h>
 #include <memoryManager.h>
+#include <scheduler.h>
+#include <timer.h>
 
 static double freq = 65536 / 3600.0; // interruptions/second
 static unsigned long ticks = 0;
 
-typedef struct SleptProcess{
+typedef struct SleptProcess {
   unsigned long ticksRemaining;
   const PCB* process;
   struct SleptProcess* next;
-}SleptProcess;
+} SleptProcess;
 
 SleptProcess* first = NULL;
 
 void incTicks() {
-    ticks++;
-    SleptProcess* currentProcess = first;
-    SleptProcess* previousProcess = NULL;
+  ticks++;
+  SleptProcess* currentProcess = first;
+  SleptProcess* previousProcess = NULL;
 
-    while (currentProcess != NULL) {
-        (currentProcess->ticksRemaining)--;
+  while (currentProcess != NULL) {
+    (currentProcess->ticksRemaining)--;
 
-        if (currentProcess->ticksRemaining == 0) {
-            readyProcess(currentProcess->process);
+    if (currentProcess->ticksRemaining == 0) {
+      readyProcess(currentProcess->process);
 
-            if (previousProcess == NULL) {
-                first = currentProcess->next;
-            } else {
-                previousProcess->next = currentProcess->next;
-            }
+      if (previousProcess == NULL) {
+        first = currentProcess->next;
+      } else {
+        previousProcess->next = currentProcess->next;
+      }
 
-            SleptProcess* toFree = currentProcess;
-            currentProcess = currentProcess->next;
-            free(toFree);
-        } else {
-            previousProcess = currentProcess;
-            currentProcess = currentProcess->next;
-        }
+      SleptProcess* toFree = currentProcess;
+      currentProcess = currentProcess->next;
+      free(toFree);
+    } else {
+      previousProcess = currentProcess;
+      currentProcess = currentProcess->next;
     }
+  }
 }
-
 
 unsigned long getTicks() {
   return ticks;
@@ -51,13 +50,13 @@ unsigned long getMs() {
 }
 
 unsigned long calcTicks(unsigned long ms) {
-  return (ms / 1000) * freq;
+  return (ms / 1000.0) * freq;
 }
 
 void sleep(unsigned long ms) {
   unsigned long initialTicks = calcTicks(ms);
-  if(initialTicks > 0){
-    SleptProcess * aux = malloc(sizeof(*aux));
+  if (initialTicks > 0) {
+    SleptProcess* aux = malloc(sizeof(*aux));
     aux->ticksRemaining = initialTicks;
     aux->process = getCurrentPCB();
     aux->next = first;
