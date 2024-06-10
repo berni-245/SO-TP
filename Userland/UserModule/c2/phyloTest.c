321df73247aa1b284ae51bd2a78d6b956b4b2f1c
FedeEtchegorry <115056584+FedeEtchegorry@users.noreply.github.com>
2024-06-09 23:51:49 -0300

    now phylo is working estoy FILOso

--- Userland/UserModule/c2/phyloTest.c
+++ Userland/UserModule/c2/phyloTest.c
@@ -1,25 +1,26 @@
 #include <shellUtils.h>
 #include <utils.h>
 
-#define PHILO_AMOUNT 5
+#define PHILO_AMOUNT 4
 #define PHILO_MAX_AMOUNT 10
 #define PHILO_MIN_AMOUNT 2
 
-#define PHYLO_MUTEX "phylo"
+#define PHYLO_MUTEX "blockAll"
 #define PHILO_NAME_LENGTH 5
 
 
 int philos_on_table;
 char* philo_name[PHILO_MAX_AMOUNT];
-int philo_state[PHILO_AMOUNT];
-int forks[PHILO_AMOUNT];
-uint64_t pids[PHILO_AMOUNT];
-
+int philo_state[PHILO_MAX_AMOUNT];
+int forks[PHILO_MAX_AMOUNT];
+uint64_t pids[PHILO_MAX_AMOUNT];
+int change_philos_sem;
 
 enum phylo{
   EATING,
   HUNGRY,
-  THINKING
+  THINKING,
+  NOTHING
 };
 
 int right_fork(int i){
@@ -28,25 +29,27 @@ int right_fork(int i){
 int left_fork(int i){
   return (i-1)%philos_on_table;
 }
-
-void change_philo_action(int value, int pos, int mutex_sem){
-  sysWaitSem(mutex_sem);
-  philo_state[pos]=value;
-  sysPostSem(mutex_sem);
+void blockAll(){
+  for (int i = 0; i < philos_on_table; ++i) {
+    sysWaitSem(change_philos_sem);
+  }
 }
 
-void monitor(int mutex_sem){
-  sysWaitSem(mutex_sem);
+void freeAll(){
+  for (int i = 0; i < philos_on_table; ++i) {
+    sysPostSem(change_philos_sem);
+  }
+}
+
+void monitor(){
   for (int i = 0; i < philos_on_table; i++) {
     printf("%c ", philo_state[i]? '.':'E');
   }
-  sysPostSem(mutex_sem);
   printf("\n");
 }
 
-
-void take_forks(int i, int mutex_sem){
-  change_philo_action(HUNGRY,i, mutex_sem);
+void take_forks(int i){
+  philo_state[i]=HUNGRY;
   if(i % 2){
     sysWaitSem(forks[right_fork(i)]);
     sysWaitSem(forks[i]);
@@ -58,15 +61,17 @@ void take_forks(int i, int mutex_sem){
 void leave_forks(int i){
   sysPostSem(forks[i]);
   sysPostSem(forks[right_fork(i)]);
+  philo_state[i]=NOTHING;
 }
-void think(int i, int mutex_sem){
-  change_philo_action(THINKING,i, mutex_sem);
-  sysSleep(randBetween(1000, 2000));
+
+void think(int i){
+  philo_state[i]=THINKING;
+  sysSleep(randBetween(400, 800));
 }
-void eat(int i, int mutex_sem){
-  change_philo_action(EATING,i, mutex_sem);
-  sysSleep(randBetween(1000, 2000));
-  monitor(mutex_sem);
+void eat(int i){
+  philo_state[i]=EATING;
+  sysSleep(randBetween(400, 800));
+  monitor();
 }
 
 
@@ -75,17 +80,14 @@ void my_phylo_process(uint64_t argc, char* argv[argc]) {
   if (n < 0) {
     sysExit(ILLEGAL_ARGUMENT);
   }
-  int mutex_sem = sysOpenSem(PHYLO_MUTEX, 1);
-  if (mutex_sem < 0) {
-    printf("my_process_inc: ERROR opening semaphore\n");
-    sysExit(MISSING_ARGUMENTS);
-  }
 
   while(1) {
-    think(n, mutex_sem);
-    take_forks(n, mutex_sem);
-    eat(n, mutex_sem);
+    sysWaitSem(change_philos_sem);
+    think(n);
+    take_forks(n);
+    eat(n);
     leave_forks(n);
+    sysPostSem(change_philos_sem);
   }
   sysExit(SUCCESS);
 }
@@ -96,14 +98,14 @@ int check_phylo_pos(int pos){
 }
 
 
-int add_Phylo(int pos){
+int add_phylo(int pos){
   if (check_phylo_pos(pos))return -1;
-  int mutex_sem= sysOpenSem(PHYLO_MUTEX, 1);
-  sysWaitSem(mutex_sem);
+  blockAll();
   char sem_name[NAME_MAX_LEN]="ph_a";
   sem_name[3]=sem_name[3]+pos;
   forks[pos]=sysCreateSemaphore(sem_name, 1);
   if(forks[pos]==-1){
+    printf("Failed creating semaphore \n");
     sysExit(PROCESS_FAILURE);
   }
   philo_name[pos]= sysMalloc(MAX_NAME_LENGTH);
@@ -114,40 +116,39 @@ int add_Phylo(int pos){
   const char* argvPhylo[] = {"my_phylo_process", phylo_num};
   printf("Philosopher number %d has joined the game\n", pos);
   philos_on_table++;
-  sysPostSem(mutex_sem);
-
+  freeAll();
   pids[pos] = sysCreateProcess(sizeof(argvPhylo) / sizeof(argvPhylo[0]), argvPhylo, my_phylo_process);
   return 0;
 }
-int rem_Phylo(int pos){
+int rem_phylo(int pos){
   if (check_phylo_pos(pos))return -1;
-  int mutex_sem= sysOpenSem(PHYLO_MUTEX, 1);
-  sysWaitSem(mutex_sem);
-  --philos_on_table;
+  blockAll();
   if (philo_state[pos] == EATING){
-    leave_forks(pos);
+    freeAll();
   }
+  philos_on_table--;
   if (sysDestroySemaphore(philo_name[pos]) == -1)
     printf("Error destroying Philosopher´s semaphore.\n");
   sysFree(philo_name[pos]);
   sysKill(pids[pos]);
-  sysPostSem(mutex_sem);
+  freeAll();
+  printf("Philosopher number %d has left the game\n", philos_on_table);
   return 0;
 }
 void end_phylos(){
-  int mutex_sem= sysOpenSem(PHYLO_MUTEX, 1);
+  blockAll();
   while (philos_on_table > 0){
-    sysWaitSem(mutex_sem);
     if (sysDestroySemaphore(philo_name[philos_on_table-1]) == -1 || !sysKill(pids[philos_on_table - 1])){
       printf("Error deleting phylo\n");
       sysExit(PROCESS_FAILURE);
     }
     philos_on_table--;
-    sysPostSem(mutex_sem);
+    printf("Philosopher number %d has left the game\n", philos_on_table);
   }
+
   if (sysDestroySemaphore(PHYLO_MUTEX)==-1){
     printf("Error deleting mutex\n");
-    return;
+    sysExit(PROCESS_FAILURE);
   }
   printf("Phylo has been ended\n");
   sysExit(SUCCESS);
@@ -167,8 +168,9 @@ void commandPhylo(int argc, char* argv[argc]) {
     strcpy(philo_name[i], sem_name);
     sem_name[3]=sem_name[3]+1;
   }
-  int mutex_sem=sysCreateSemaphore(PHYLO_MUTEX, 1);
-  sysWaitSem(mutex_sem);
+
+  change_philos_sem = sysCreateSemaphore(PHYLO_MUTEX, PHILO_AMOUNT);
+  blockAll();
   for (int i = 0; i < PHILO_AMOUNT; i++) {
     char philo_num[3];
     uintToBase(i, philo_num, 10);
@@ -178,23 +180,21 @@ void commandPhylo(int argc, char* argv[argc]) {
     philos_on_table++;
     printf("Philosopher number %d has joined the game\n", i);
   }
-  sysPostSem(mutex_sem);
+  freeAll();
 
   KeyStruct key;
   while (1) {
     sysHalt();
     if (getKey(&key) != EOF){
       if (key.character == 'a' || key.character=='A'){
-        add_Phylo(philos_on_table);
+        add_phylo(philos_on_table);
       }
       else if (key.character == 'r' || key.character=='R'){
-        rem_Phylo(philos_on_table-1);
+        rem_phylo(philos_on_table-1);
       }
       else if (key.character == 'e' || key.character=='E'){
         end_phylos();
       }
     }
   }
-
-
 }
