284373ad7c7e1a64f1830a97a9e28ac8ae896e78
Franco Morroni <fmorroni99@gmail.com>
2024-06-14 16:50:17 -0300

    feat: better mode for processes test

--- Userland/UserModule/c2/testProcesses.c
+++ Userland/UserModule/c2/testProcesses.c
@@ -1,3 +1,4 @@
+#include <processes.h>
 #include <shellUtils.h>
 #include <stdint.h>
 #include <syscalls.h>
@@ -10,6 +11,14 @@ typedef struct P_rq {
   enum State state;
 } p_rq;
 
+void printUsage(char* procName) {
+  printf("Usage:\n\t%s <max_processes> <print_status_changes> <ps_and_step>\n", procName);
+  printf("\t\tmax_processes: natural number\n");
+  printf("\t\tprint_status_changes: 1 to print, 0 to hide\n");
+  printf("\t\tps_and_step: omit to deactivate, anything to activate. "
+         "Print real process list after each iteration and wait for input before continuing\n");
+}
+
 void commandTestProcesses(int32_t argc, char* argv[]) {
   uint8_t rq;
   uint8_t alive = 0;
@@ -17,16 +26,18 @@ void commandTestProcesses(int32_t argc, char* argv[]) {
   uint64_t max_processes;
   const char* argvAux[] = {"endless_loop"};
 
-  if (argc != 3) {
-    printf("Usage: %s <max_processes> <print_status_changes(1 to print, 0 to hide)>\n", argv[0]);
+  if (argc < 3) {
+    printUsage(argv[0]);
     sysExit(MISSING_ARGUMENTS);
   }
 
   int print_is_active = satoi(argv[2]);
   if ((max_processes = satoi(argv[1])) <= 0 || (print_is_active != 1 && print_is_active != 0)) {
-    printf("Usage: %s <max_processes> <print_status_changes(1 to print, 0 to hide)>\n", argv[0]);
+    printUsage(argv[0]);
     sysExit(ILLEGAL_ARGUMENT);
   }
+
+  bool printPs = argc > 3;
 
   p_rq p_rqs[max_processes];
 
@@ -78,7 +89,7 @@ void commandTestProcesses(int32_t argc, char* argv[]) {
       }
 
       // Randomly unblocks processes
-      for (rq = 0; rq < max_processes; rq++)
+      for (rq = 0; rq < max_processes; rq++) {
         if (p_rqs[rq].state == BLOCKED && GetUniform(100) % 2) {
           if (!sysUnblock(p_rqs[rq].pid)) {
             printf("test_processes: ERROR unblocking process\n");
@@ -87,6 +98,15 @@ void commandTestProcesses(int32_t argc, char* argv[]) {
           if (print_is_active) printf("Process (PID=%d) was unblocked successfully\n", p_rqs[rq].pid);
           p_rqs[rq].state = RUNNING;
         }
+      }
+
+      if (printPs) {
+        int len;
+        PCB* pcbList = sysPCBList(&len);
+        printPCBList(pcbList, len);
+        sysFree(pcbList);
+        getChar();
+      }
     }
     printf("%d processes alive. Execution of testProcesses ended successfully\n", alive);
     sysExit(SUCCESS);
